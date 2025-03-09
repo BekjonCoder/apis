c0e7059e8b10bb86bec2ef50a3cccf0b56e380d3
BekjonCoder <k75154256@gmail.com>
2025-03-09 09:58:24 +0500

    sds

--- script.js
+++ script.js
@@ -1,134 +1,217 @@
-const countriesContainer=document.querySelector('.container') 
+// const countriesContainer=document.querySelector('.container') 
 
 
-const getCountryData=function(country){
-    const request=new XMLHttpRequest()
-request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// const getCountryData=function(country){
+//     const request=new XMLHttpRequest()
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 
-request.send()
-request.addEventListener('load',()=>{
-    const [info]=JSON.parse(request.responseText)
-    console.log(info);
+// request.send()
+// request.addEventListener('load',()=>{
+//     const [info]=JSON.parse(request.responseText)
+//     console.log(info);
     
-    const html=`
-     <article class="country">
-                <img class="country__img" src="${info.flags.png}" alt="${info.flags.alt}">
-                <div class="country__data">
-                    <h3 class="country__name">${info.name.common}</h3>
-                    <h4 class="country__region">${info.region}</h4>
-                    <p class="country__row"><span>🧍‍♀️🧍‍♂️ </span>${(info.population/1000000).toFixed(1)}</p>
-                    <p class="country__row"><span>Capital: </span>${info.capital[0]}</p>
-                    <p class="country__row"><span>StartOfWeek: </span>${info.borders[0], info.startOfWeek}</p>
-                </div>
-            </article>
-            `
-            countriesContainer.insertAdjacentHTML('beforeend',html)
-            countriesContainer.style.opacity=1
-})
-}
-getCountryData('uzbekistan')
-getCountryData('arab')
-getCountryData('italy')
-getCountryData('russian')
-getCountryData('germany')
-getCountryData('france')
-getCountryData('united')
-getCountryData('united kingdom')
-getCountryData('Afghanistan');
-getCountryData('Albania');
-getCountryData('Algeria');
-getCountryData('Andorra');
-getCountryData('Angola');
-getCountryData('Argentina');
-getCountryData('Armenia');
-getCountryData('Australia');
-getCountryData('Austria');
-getCountryData('Azerbaijan');
-getCountryData('Bahamas');
-getCountryData('Bahrain');
-getCountryData('Bangladesh');
-getCountryData('Barbados');
-getCountryData('Belarus');
-getCountryData('Belgium');
-getCountryData('Belize');
-getCountryData('Benin');
-getCountryData('Bhutan');
-getCountryData('Bolivia');
-getCountryData('Bosnia and Herzegovina');
-getCountryData('Botswana');
-getCountryData('Brazil');
-getCountryData('Brunei');
-getCountryData('Bulgaria');
-getCountryData('Burkina Faso');
-getCountryData('Burundi');
-getCountryData('Cambodia');
-getCountryData('Cameroon');
-getCountryData('Canada');
-getCountryData('Cape Verde');
-getCountryData('Central African Republic');
-getCountryData('Chad');
-getCountryData('Chile');
-getCountryData('China');
-getCountryData('Colombia');
-getCountryData('Comoros');
-getCountryData('Congo');
-getCountryData('Costa Rica');
-getCountryData('Croatia');
-getCountryData('Cuba');
-getCountryData('Cyprus');
-getCountryData('Czech Republic');
-getCountryData('Denmark');
-getCountryData('Djibouti');
-getCountryData('Dominica');
-getCountryData('Dominican Republic');
-getCountryData('Ecuador');
-getCountryData('Egypt');
-getCountryData('El Salvador');
-getCountryData('Equatorial Guinea');
-getCountryData('Eritrea');
-getCountryData('Estonia');
-getCountryData('Eswatini');
-getCountryData('Ethiopia');
-getCountryData('Fiji');
-getCountryData('Finland');
+//     const html=`
+//      <article class="country">
+//                 <img class="country__img" src="${info.flags.png}" alt="${info.flags.alt}">
+//                 <div class="country__data">
+//                     <h3 class="country__name">${info.name.common}</h3>
+//                     <h4 class="country__region">${info.region}</h4>
+//                     <p class="country__row"><span>🧍‍♀️🧍‍♂️ </span>${(info.population/1000000).toFixed(1)}</p>
+//                     <p class="country__row"><span>Capital: </span>${info.capital[0]}</p>
+//                     <p class="country__row"><span>StartOfWeek: </span>${info.borders[0], info.startOfWeek}</p>
+//                 </div>
+//             </article>
+//             `
+//             countriesContainer.insertAdjacentHTML('beforeend',html)
+//             countriesContainer.style.opacity=1
+// })
+// }
+
+// const countryInput=document.getElementById('countryInput')
+// countryInput.addEventListener('input',()=>{
+//     const inputValue=countryInput.value
+//     const getCountryData=function(country){
+//         const request=new XMLHttpRequest()
+//     request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    
+//     request.send()
+//     request.addEventListener('load',()=>{
+//         const [info]=JSON.parse(request.responseText)
+//         console.log(info);
+        
+//         const html=`
+//          <article class="country">
+//                     <img class="country__img" src="${info.flags.png}" alt="${info.flags.alt}">
+//                     <div class="country__data">
+//                         <h3 class="country__name">${info.name.common}</h3>
+//                         <h4 class="country__region">${info.region}</h4>
+//                         <p class="country__row"><span>🧍‍♀️🧍‍♂️ </span>${(info.population/1000000).toFixed(1)}</p>
+//                         <p class="country__row"><span>Capital: </span>${info.capital[0]}</p>
+//                         <p class="country__row"><span>StartOfWeek: </span>${info.borders[0], info.startOfWeek}</p>
+//                     </div>
+//                 </article>
+//                 `
+//                 countriesContainer.insertAdjacentHTML('beforeend',html)
+//                 countriesContainer.style.opacity=1
+//     })
+//     }})
+
+
+//     getCountryData('uzbekistan')
+// getCountryData('arab')
+// getCountryData('italy')
+// getCountryData('russian')
+// getCountryData('germany')
+// getCountryData('france')
+// getCountryData('united')
+// getCountryData('united kingdom')
+// getCountryData('Afghanistan');
+// getCountryData('Albania');
+// getCountryData('Algeria');
+// getCountryData('Andorra');
+// getCountryData('Angola');
+// getCountryData('Argentina');
+// getCountryData('Armenia');
+// getCountryData('Australia');
+// getCountryData('Austria');
+// getCountryData('Azerbaijan');
+// getCountryData('Bahamas');
+// getCountryData('Bahrain');
+// getCountryData('Bangladesh');
+// getCountryData('Barbados');
+// getCountryData('Belarus');
+// getCountryData('Belgium');
+// getCountryData('Belize');
+// getCountryData('Benin');
+// getCountryData('Bhutan');
+// getCountryData('Bolivia');
+// getCountryData('Bosnia and Herzegovina');
+// getCountryData('Botswana');
+// getCountryData('Brazil');
+// getCountryData('Brunei');
+// getCountryData('Bulgaria');
+// getCountryData('Burkina Faso');
+// getCountryData('Burundi');
+// getCountryData('Cambodia');
+// getCountryData('Cameroon');
+// getCountryData('Canada');
+// getCountryData('Cape Verde');
+// getCountryData('Central African Republic');
+// getCountryData('Chad');
+// getCountryData('Chile');
+// getCountryData('China');
+// getCountryData('Colombia');
+// getCountryData('Comoros');
+// getCountryData('Congo');
+// getCountryData('Costa Rica');
+// getCountryData('Croatia');
+// getCountryData('Cuba');
+// getCountryData('Cyprus');
+// getCountryData('Czech Republic');
+// getCountryData('Denmark');
+// getCountryData('Djibouti');
+// getCountryData('Dominica');
+// getCountryData('Dominican Republic');
+// getCountryData('Ecuador');
+// getCountryData('Egypt');
+// getCountryData('El Salvador');
+// getCountryData('Equatorial Guinea');
+// getCountryData('Eritrea');
+// getCountryData('Estonia');
+// getCountryData('Eswatini');
+// getCountryData('Ethiopia');
+// getCountryData('Fiji');
+// getCountryData('Finland');
+// getCountryData('France');
+// getCountryData('Gabon');
+// getCountryData('Gambia');
+// getCountryData('Georgia');
+// getCountryData('Ghana');
+// getCountryData('Greece');
+// getCountryData('Grenada');
+// getCountryData('Guatemala');
+// getCountryData('Guinea');
+// getCountryData('Guinea-Bissau');
+// getCountryData('Guyana');
+// getCountryData('Haiti');
+// getCountryData('Honduras');
+// getCountryData('Hungary');
+// getCountryData('Iceland');
+// getCountryData('India');
+// getCountryData('Indonesia');
+// getCountryData('Iran');
+// getCountryData('Iraq');
+// getCountryData('Ireland');
+// getCountryData('Jamaica');
+// getCountryData('Japan');
+// getCountryData('Jordan');
+// getCountryData('Kazakhstan');
+// getCountryData('Kenya');
+// getCountryData('Kiribati');
+// getCountryData('Kuwait');
+// getCountryData('Kyrgyzstan');
+// getCountryData('Laos');
+// getCountryData('Latvia');
+// getCountryData('Lebanon');
+// getCountryData('Lesotho');
+// getCountryData('Liberia');
+// getCountryData('Libya');
+// getCountryData('Liechtenstein');
+// getCountryData('Lithuania');
+// getCountryData('Luxembourg');
+// getCountryData('Madagascar');
+// getCountryData('Malawi');
+// getCountryData('Malaysia');
+// getCountryData('Liberia');
+const countriesContainer = document.querySelector('.container');
+
+// Mamlakat ma'lumotlarini olish funksiyasi
+const getCountryData = function (country) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+
+    request.addEventListener('load', () => {
+        const [info] = JSON.parse(request.responseText);
+        console.log(info);
+
+        const html = `
+         <article class="country">
+            <img class="country__img" src="${info.flags.png}" alt="Country Flag">
+            <div class="country__data">
+                <h3 class="country__name">${info.name.common}</h3>
+                <h4 class="country__region">${info.region}</h4>
+                <p class="country__row"><span>🧍‍♀️🧍‍♂️ </span>${(info.population / 1000000).toFixed(1)} mln</p>
+                <p class="country__row"><span>Capital: </span>${info.capital ? info.capital[0] : 'No Capital'}</p>
+                <p class="country__row"><span>StartOfWeek: </span>${info.startOfWeek}</p>
+            </div>
+         </article>
+        `;
+
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = 1;
+    });
+};
+
+// Input orqali mamlakat qidirish
+const countryInput = document.getElementById('countryInput');
+countryInput.addEventListener('input', () => {
+    const inputValue = countryInput.value.trim(); // Bo‘sh joylarni olib tashlash
+    if (inputValue !== '') {
+        countriesContainer.innerHTML = ''; // Avvalgi ma'lumotlarni o‘chirish
+        getCountryData(inputValue);
+    }
+});
+
+// Dastlab ba'zi mamlakatlarni yuklash
+getCountryData('Uzbekistan');
+getCountryData('Russia');
+getCountryData('Italy');
+getCountryData('Germany');
 getCountryData('France');
-getCountryData('Gabon');
-getCountryData('Gambia');
-getCountryData('Georgia');
-getCountryData('Ghana');
-getCountryData('Greece');
-getCountryData('Grenada');
-getCountryData('Guatemala');
-getCountryData('Guinea');
-getCountryData('Guinea-Bissau');
-getCountryData('Guyana');
-getCountryData('Haiti');
-getCountryData('Honduras');
-getCountryData('Hungary');
-getCountryData('Iceland');
-getCountryData('India');
-getCountryData('Indonesia');
-getCountryData('Iran');
-getCountryData('Iraq');
-getCountryData('Ireland');
-getCountryData('Jamaica');
+getCountryData('United Kingdom');
+getCountryData('United States');
+getCountryData('Egypt');
 getCountryData('Japan');
-getCountryData('Jordan');
+getCountryData('India');
 getCountryData('Kazakhstan');
-getCountryData('Kenya');
-getCountryData('Kiribati');
-getCountryData('Kuwait');
-getCountryData('Kyrgyzstan');
-getCountryData('Laos');
-getCountryData('Latvia');
-getCountryData('Lebanon');
-getCountryData('Lesotho');
-getCountryData('Liberia');
-getCountryData('Libya');
-getCountryData('Liechtenstein');
-getCountryData('Lithuania');
-getCountryData('Luxembourg');
-getCountryData('Madagascar');
-getCountryData('Malawi');
-getCountryData('Malaysia');
-getCountryData('Liberia');
